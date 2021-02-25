7ca33b90bb5a95b2661fb4430b4b78d0fbc891c6
Rafa menegon <dev.rafaelmenegon@gmail.com>
2021-02-25 14:06:09 -0300

    Day 4: Notification sound

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -55,6 +55,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
     setActiveChallenge(challenge);
 
+    new Audio('/notification.mp3').play();
+
     if (Notification.permission === 'granted') {
       new Notification('Novo desafio 🎉', {
         body: `Valendo ${challenge.amount}xp!`
